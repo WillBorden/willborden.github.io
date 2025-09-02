1f79205c6dd041d705760ece351d7f97e7daceb9
WillBorden <102001283+WillBorden@users.noreply.github.com>
2025-09-02 06:17:04 +0000

    Deploying to gh-pages from @ WillBorden/willborden.github.io@6aeef8ae30f9937b0754d558ecbc643d15154a8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "Check out some of my projects!",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
