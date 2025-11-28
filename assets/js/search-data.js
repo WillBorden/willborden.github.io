2260be64a79fd9c7a1a10273d8db30e30ffbe15f
WillBorden <102001283+WillBorden@users.noreply.github.com>
2025-11-28 11:19:18 +0000

    Deploying to gh-pages from @ WillBorden/willborden.github.io@efa7373018ea39868339c0ac818e7f3614f124a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "projects-fender-champ-amp-replica",
+        },{id: "projects-fender-champ-amp-replica",
           title: 'Fender Champ Amp replica',
           description: "with background image",
           section: "Projects",handler: () => {
